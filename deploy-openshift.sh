bd407de8ed7e0411e30994486bc64c75bcecbb2a
Samuel Vandamme <samuel@sava.be>
2018-04-11 15:45:52 +0200

    Added missing namespace for Openshift

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -9,13 +9,13 @@ NAMESPACE=${1:-coscale-micro}
 oadm new-project $NAMESPACE
 oc project $NAMESPACE
 oadm policy add-scc-to-user anyuid -z default
-oc apply -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/mysql.yaml
-oc apply -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/rabbitmq.yaml
-oc apply -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/redis.yaml
-oc apply -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/receiver.yaml
-oc apply -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/web.yaml
-oc apply -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/services.yaml
-oc apply -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/calc_letters.yaml
-oc apply -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/calc_words.yaml
-oc apply -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/generator.yaml
-oc apply -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/processor.yaml
+oc apply -n $NAMESPACE -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/mysql.yaml
+oc apply -n $NAMESPACE -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/rabbitmq.yaml
+oc apply -n $NAMESPACE -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/redis.yaml
+oc apply -n $NAMESPACE -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/receiver.yaml
+oc apply -n $NAMESPACE -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/web.yaml
+oc apply -n $NAMESPACE -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/services.yaml
+oc apply -n $NAMESPACE -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/calc_letters.yaml
+oc apply -n $NAMESPACE -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/calc_words.yaml
+oc apply -n $NAMESPACE -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/generator.yaml
+oc apply -n $NAMESPACE -f https://raw.githubusercontent.com/kidk/training-images/master/kubernetes/processor.yaml
